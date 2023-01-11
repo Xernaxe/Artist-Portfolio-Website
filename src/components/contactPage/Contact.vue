efc62e5f75ef9e1def74b721a60d27317f311f7e
Xernaxe <93713092+Xernaxe@users.noreply.github.com>
2023-01-12 00:34:59 +0100

    asd

--- src/components/contactPage/Contact.vue
+++ src/components/contactPage/Contact.vue
@@ -1,5 +1,5 @@
 <template>
-  
+  <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdG_eDgaA8TIgbTravr3vHrMd-SPdHfCWb0BjzbK3B-YpDkKQ/viewform?embedded=true" width="640" height="674" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
 </template>
 
 <script>
--- src/components/storyPage/Essay.vue
+++ src/components/storyPage/Essay.vue
@@ -3,15 +3,16 @@
     <h2 class="sectionH">I also like writing essays</h2>
     <p class="essayP">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer.</p>
     <div class="essayEMB">
-      PDF EMBEDED
+      <a href="https://online.flippingbook.com/view/222203433/" src="https://online.flippingbook.com/EmbedScriptUrl.aspx?m=redir&hid=222203433" class="fbo-embed" data-fbo-id="020200c4fd" data-fbo-ratio="3:2" data-fbo-lightbox="yes" data-fbo-width="100%" data-fbo-height="auto" data-fbo-version="1" style="max-width: 100%">Sunset-March-and-my-compositional-process</a>
     </div>
   </section>
 </template>
 
 <script>
 export default {
-
+  
 }
+
 </script>
 
 <style scoped>
