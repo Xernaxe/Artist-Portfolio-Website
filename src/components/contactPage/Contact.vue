cde7af8109007f1f9855c1f3fdefb8e5148cd42d
b1511 <borislava.ph@gmail.com>
2023-01-12 01:02:55 +0100

    spacing

--- src/components/contactPage/Contact.vue
+++ src/components/contactPage/Contact.vue
@@ -1,5 +1,5 @@
 <template>
-  <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdG_eDgaA8TIgbTravr3vHrMd-SPdHfCWb0BjzbK3B-YpDkKQ/viewform?embedded=true" width="640" height="674" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+  <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdG_eDgaA8TIgbTravr3vHrMd-SPdHfCWb0BjzbK3B-YpDkKQ/viewform?embedded=true" width="640" height="850" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
 </template>
 
 <script>
@@ -8,6 +8,8 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+iframe {
+  margin-top: 60px;
+}
 </style>
