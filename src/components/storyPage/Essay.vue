ac3a98a778c4cd2eed133732c69a4113b618711b
b1511 <borislava.ph@gmail.com>
2023-01-13 17:50:55 +0100

    essay link

--- src/components/storyPage/Essay.vue
+++ src/components/storyPage/Essay.vue
@@ -3,7 +3,7 @@
     <h2 class="sectionH">Reflections, essays and poetry</h2>
     <p class="essayP">“Once a while, I still enjoy writing. In 2020, I finished my first collection of poetry and once in a while, I enjoy reflecting on my creative process and on musicians’ life in general. Some of my texts are available here.”</p>
     <div class="essayEMB">
-      <a href="https://online.flippingbook.com/view/222203433/" src="https://online.flippingbook.com/EmbedScriptUrl.aspx?m=redir&hid=222203433" class="fbo-embed" data-fbo-id="020200c4fd" data-fbo-ratio="3:2" data-fbo-lightbox="yes" data-fbo-width="100%" data-fbo-height="auto" data-fbo-version="1" style="max-width: 100%">Sunset-March-and-my-compositional-process</a>
+      <a href="https://drive.google.com/file/d/1ml1ui6XEnzH_dmKhXBdoKf7JVQoFDKaH/view?usp=share_link" target="_blank">Sunset-March-and-my-compositional-process</a>
     </div>
   </section>
 </template>
