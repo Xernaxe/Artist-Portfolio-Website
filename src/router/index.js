6ae74c4df5c4709db69037061baed31c4a0a6a8a
Xernaxe <93713092+Xernaxe@users.noreply.github.com>
2023-02-07 16:14:17 +0100

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -7,9 +7,6 @@ import StoryView from '../views/StoryView.vue'
 
 
 const router = createRouter({
-  scrollBehavior(to, from, savedPosition) {
-    return { top: 0 }
-  },
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
