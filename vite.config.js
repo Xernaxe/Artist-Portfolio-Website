d3e71c8868b5744e54f70853097606c84c8d337f
Xernaxe <93713092+Xernaxe@users.noreply.github.com>
2023-02-13 13:27:23 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/class/MDE-CSD-S21/10407762/ArtistPortfolioProjectV2',
+  // base: '/class/MDE-CSD-S21/10407762/ArtistPortfolioProjectV2',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
